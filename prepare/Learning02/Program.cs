a8b3fe25191c9a42ccacc098619ad67553ed1be0
mrbeals <sbeals2@byui.edu>
2025-10-01 16:07:40 -0700

    Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Papa Johns";
+        job1._jobTitle = "Shift Leader";
+        job1._startYear = 2017;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Math Teacher";
+        job2._company = "Brightmont Academy";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume resume = new Resume();
+        resume._name = "Spencer Beals";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+
+public class Resume
+{
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Job: ");
+        
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
