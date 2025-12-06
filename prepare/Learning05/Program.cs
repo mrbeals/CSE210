0924ab9e56373477967680b24583db656ece13b5
mrbeals <sbeals2@byui.edu>
2025-12-05 16:20:30 -0800

    Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Rectangle rectangle = new Rectangle("green", 12.2, 13.2);
+        Square square = new Square("red", 10);
+        Circle circle = new Circle("pink", 10);
+
+        shapes.Add(rectangle);
+        shapes.Add(square);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"For the {color} shape, the area is {area}");
+        }
     }
 }
